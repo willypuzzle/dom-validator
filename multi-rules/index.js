ff869fda26f1e9ae53940a0c8251aeca0c26dd99
Domenico Rizzo <dev@idsign.it>
2017-12-21 18:05:45 +0100

    Corrected some dummy bug

--- multi-rules/index.js
+++ multi-rules/index.js
@@ -4,10 +4,12 @@ import between_number_of from './between_number_of'
 import min_number_of from './min_number_of'
 import max_number_of from './max_number_of'
 import number_of from './number_of'
+import dummy from './dummy'
 
 
 export default {
   between_number_of,
+  dummy,
   in_values: In,
   min_number_of,
   max_number_of,
