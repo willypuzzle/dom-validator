f38cee48bab27d94921444f874bd2abaacd9eae8
Domenico Rizzo <dev@idsign.it>
2018-01-05 11:37:39 +0100

    corrected date rule

--- rules/date.js
+++ rules/date.js
@@ -29,21 +29,32 @@ let check = (value, params, scope, otherComponentsInScope, obj, fieldName, local
     let operator = params[1];
     let delta = params[2];
 
-    return dateCheck(date, operator, delta);
+    return dateCheck(date, operator, delta, params, locale, otherComponentsInScope);
 
 };
 
-let dateCheck = (date, operator, delta) => {
+let dateCheck = (date, operator, delta, params, locale, otherComponentsInScope) => {
+    let operatorArray = operator.split('-')
+    let op = operatorArray[0];
+    let field = null;
+    if(operatorArray.length > 1){
+        field = operatorArray[1];
+    }
+
     date = moment(date).format('YYYY-MM-DD');;
     let second = null
     if(delta === 'now'){
         second = moment().format('YYYY-MM-DD')
     }else {
-        second = buildSecond(delta)
+        second = buildSecond(params, locale, delta, field, otherComponentsInScope)
+    }
+
+    if(!second){
+        return false;
     }
 
 
-    switch (operator){
+    switch (op){
         case '>':
             return date > second;
         case '<':
@@ -53,30 +64,57 @@ let dateCheck = (date, operator, delta) => {
         case '<=':
             return date <= second;
         case '=':
-            return date = second;
+            return date === second;
         default:
             throw 'Dom-validator: date rule, invalid operator \'' + operator + '\''
     }
 }
 
-let buildSecond = (deltax) => {
+let buildSecond = (params, locale, deltax, field, otherComponentsInScope) => {
     let delta = deltax.split('-');
-    if(delta.length !== 3){
-        throw `Dom-validator: date rule invalid delta '${deltax}', it has to be in the format 'rule-value-unit' for example 'before-20-years' or 'now'`
+    if(delta.length !== 3 && delta.length === 1 && delta[0] !== 'it'){
+        throw `Dom-validator: date rule invalid delta '${deltax}', it has to be in the format 'rule-value-unit' for example 'before-20-years', 'now' or 'it'`
     }
     let rule = delta[0];
-    let value = delta[1];
-    let unit = delta[2];
+    let value = delta[1] || null;
+    let unit = delta[2] || null;
+
+    let referringTime = buildReferringTime(params, locale, field, otherComponentsInScope)
+    if(!referringTime){
+        return false;
+    }
 
     switch (rule){
         case 'before':
-            return moment().subtract(parseInt(value), unit).format('YYYY-MM-DD');
+            return moment(referringTime).subtract(parseInt(value), unit).format('YYYY-MM-DD');
         case 'after':
-            return moment().add(parseInt(value), unit).format('YYYY-MM-DD');
+            return moment(referringTime).add(parseInt(value), unit).format('YYYY-MM-DD');
+        case 'it':
+            return moment(referringTime).format('YYYY-MM-DD');
         default:
             throw `Dom-validator: date rule invalid delta '${deltax}', first segment has to be 'before' or after`
 
     }
+}
+
+let buildReferringTime = (params, locale, field, otherComponentsInScope) => {
+    if(!field){
+        return moment().format('YYYY-MM-DD')
+    }
+
+    for(let index in otherComponentsInScope){
+        if(index === field){
+            let val = otherComponentsInScope[index].component.inputValue || otherComponentsInScope[index].component.lazyValue;
+            val = parseDateIntoString(val, params, locale);
+            if(val){
+                return moment(val).format('YYYY-MM-DD')
+            }else{
+                return false;
+            }
+        }
+    }
+
+    throw `Dom-validator: date rule: field '${field}' does not exist`
 }
 
 export default check;
