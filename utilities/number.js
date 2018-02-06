a175d46cc56c131c8ccb3c23fc08878db599a0ca
Domenico Rizzo <dev@idsign.it>
2018-02-06 16:57:23 +0100

    Corrected bug in number

--- utilities/number.js
+++ utilities/number.js
@@ -1,10 +1,10 @@
 let parseFloatLocale = (value, locale) => {
     switch (locale){
         case 'it':
-            value = String(value).replace('.','').replace(',','.')
+            value = String(value).replace(/\./g,'').replace(',','.')
             break;
         default:
-            value = String(value).replace(',','');
+            value = String(value).replace(/,/g,'').replace(',','');
     }
 
     return parseFloat(value);
